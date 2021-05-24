8fd64829accedf829c04577b31e443fa7f2610b2
lakshanofficial <lakshanperera625@gmail.com>
2021-05-24 22:08:47 +0530

    🌟

---
+++ basics/movie-rental.sql
@@ -0,0 +1,17 @@
+CREATE TABLE customer (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20)
+);
+
+CREATE TABLE movie (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20)
+);
+
+CREATE TABLE movie_rentals (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    movie_id INT,
+    FOREIGN KEY(customer_id) REFERENCES customer(id) ON DELETE SET NULL,
+    FOREIGN KEY(movie_id) REFERENCES movie(id) ON DELETE SET NULL
+);
